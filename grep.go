bbdf67f3da1e07a0733b8034a81ab0c4a4811b50
harsh-not-haarsh <anand.2@iitj.ac.in>
2020-04-24 00:37:16 +0530

    printing all substrings of length same as pattern

---
+++ grep.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	pattern := os.Args[1]
+	filename := os.Args[2]
+
+	searchInFile(pattern, filename)
+}
+
+func searchInFile(pattern string, filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		tokens := strings.Split(line, "")
+
+		for i := 0; i <= len(tokens)-len(pattern); i++ {
+			fmt.Print(line[i:i+len(pattern)], ",")
+		}
+
+	}
+
+	err = scanner.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
